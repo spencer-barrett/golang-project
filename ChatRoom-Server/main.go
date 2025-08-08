9459f5e876444c2213b84acee17168c5a2b5d906
spencer-barrett <barrettspencer@outlook.com>
2025-08-08 14:54:24 -0700

    Simple server/websocket implementation connecting clients

--- ChatRoom-Server/main.go
+++ ChatRoom-Server/main.go
@@ -1,9 +1,9 @@
-package ChatRoom_Server
+package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"text/template"
 
 	"github.com/gorilla/websocket"
 )
@@ -24,4 +24,70 @@ type Client struct {
 	hub  *Hub
 	conn *websocket.Conn
 	send chan Message
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+func main() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	fmt.Println("Server is running...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+	log.Println(http.ListenAndServe(":8080", nil))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	clients[conn] = true
+
+	for {
+		var msg Message
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+			log.Println(err)
+			delete(clients, conn)
+			return
+		}
+
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				fmt.Println(err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
