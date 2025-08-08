daba3d8f17ffa3a17bf04925683e1e03a9614919
john.stewart1324 <57651326+JohnStewart1324@users.noreply.github.com>
2025-08-08 14:17:13 -0700

    Implemented Message, Hub, and Client data structures

--- ChatRoom-Server/main.go
+++ ChatRoom-Server/main.go
@@ -1,1 +1,27 @@
 package ChatRoom_Server
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan Message
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan Message
+}
